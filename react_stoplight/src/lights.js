43016dd18e4a944ac83776b2b85deeb46767eb38
tussingj89 <tussing40@gmail.com>
2021-12-28 22:56:07 -0500

    update with error

--- react_stoplight/src/lights.js
+++ react_stoplight/src/lights.js
@@ -1,10 +1,51 @@
 import './lights.css'
+import React, { useState} from 'react';
+import Circles from './components/circles';
 
 
-function app() {
+const App = () => {
+  const [redLight, setredLight] = useState([]);
+  const [yellowLight, setyellowLight] = useState([]);
+  const [greenLight, setGreenLight] = useState([]);
+  const [next, setnext] = useState([]);
+  setnext("red");
+
+  handeLightChange = () => {
+    switch (next) {
+      case "red":
+        setredLight("red"),
+        setyellowLight("white"),
+        setGreenLight("white")
+        setnext("green")
+        break;
+      case "green":
+        setredLight("white"),
+        setyellowLight("white"),
+        setGreenLight("green")
+        setnext("green")
+        break;
+      case "yellow":
+        setredLight("white"),
+        setyellowLight("yellow"),
+        setGreenLight("white")
+        setnext("green")
+        break;
+    }
+
+  componentDidMount() {
+    setInterval(() => { 
+      handeLightChange();
+    }, 2000)
+  }
+};
+    
     return (
-        <h1>Traffic light</h1>
+        <><h1>Traffic light</h1>
+        <Circles color={redLight} />
+        <Circles color={yellowLight} />
+        <Circles color={greenLight} /></>
 
     );
 }
-export default app;
+
+export default App;
